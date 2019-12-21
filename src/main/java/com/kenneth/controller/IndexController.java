412e3631440525d8f574a4203c41a8e3292930ae
qinkai <15869129921@139.com>
2019-12-21 10:05:48 +0800

    提交代码

---
+++ src/main/java/com/kenneth/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.kenneth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @auther qinkai
+ * @data 2019/12/21 9:55
+ */
+@RestController
+@RequestMapping(value = "/index")
+public class IndexController {
+
+    @GetMapping
+    public String index(){
+        return "hello spring boot";
+    }
+}
